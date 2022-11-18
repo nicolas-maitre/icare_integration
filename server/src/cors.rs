ffb796f1cc0c119713699b6ae883cf0bfdcd054f
Nicolas Maitre <nicolas.maitre@lausanne.ch>
2022-11-18 12:16:07 +0100

    migrate back to rocket 0.4

--- server/src/cors.rs
+++ server/src/cors.rs
@@ -1,12 +1,8 @@
-use rocket::{
-    fairing::{Fairing, Info, Kind},
-    http::Header,
-    Request, Response,
-};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
 
 pub struct CORS;
-
-#[rocket::async_trait]
 impl Fairing for CORS {
     fn info(&self) -> Info {
         Info {
@@ -15,17 +11,17 @@ impl Fairing for CORS {
         }
     }
 
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new(
-            "Access-Control-Allow-Origin",
-            "*", //"http://127.0.0.1:5173",
-        ));
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",
-            // "POST, GET, PATCH, OPTIONS",
-            "GET",
+            "POST, GET, PATCH, OPTIONS",
+            // "GET",
         ));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Credentials",
+            "true",
+        ));
     }
 }
